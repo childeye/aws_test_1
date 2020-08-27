6818eabe556327a1189f9e49570cc52f8281b961
childeye <childeye@uangel.com>
2020-08-27 11:54:33 +0900

    init

---
+++ src/main/java/com/example/demo/aws/bootweb/AwsTestApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo.aws.bootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// https://github.com/jojoldu/freelec-springboot2-webservice
+
+
+@SpringBootApplication
+public class AwsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/aws/bootweb/service/PostsService.java
@@ -0,0 +1,60 @@
+package com.example.demo.aws.bootweb.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.aws.bootweb.domain.posts.Posts;
+import com.example.demo.aws.bootweb.domain.posts.PostsRepository;
+import com.example.demo.aws.bootweb.web.dto.PostsListResponseDto;
+import com.example.demo.aws.bootweb.web.dto.PostsResponseDto;
+import com.example.demo.aws.bootweb.web.dto.PostsSaveRequestDto;
+import com.example.demo.aws.bootweb.web.dto.PostsUpdateRequestDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
