fb45025811a2d71b8f32c55aaa1585f8694ee186
childeye <childeye@uangel.com>
2020-08-28 11:58:30 +0900

    자동 배포 & 실행.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=freelec-springboot2-webservice
+APP_HOME=/home/ec2-user/app/test_web_1
+APP_REPOSITORY=$APP_HOME/source/aws_test_1
+APP_LIB_DIR=$APP_HOME/lib
+APP_LOG_DIR=$APP_HOME/logs
+APP_CONF_DIR=$APP_HOME/conf
+APP_DEPLOY_DIR=$APP_HOME/zip
+JAR_NAME=bootweb-0.0.1-SNAPSHOT.jar
+
+cd $APP_HOME
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp APP_DEPLOY_DIR/$JAR_NAME $APP_LIB_DIR/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $JAR_NAME | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
@@ -23,17 +30,17 @@ fi
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+chmod +x $APP_LIB_DIR/$JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
+# nohup java -jar $APP_LIB_DIR/$JAR_NAME > $APP_LOG_DIR/nohup.out 2>&1 &
+
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.config.location=classpath:/application.properties,$APP_CONF_DIR/application-real.properties,$APP_CONF_DIR/application-oauth.properties,$APP_CONF_DIR/application-real-db.properties \
     -Dspring.profiles.active=real \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    $APP_LIB_DIR/$JAR_NAME > $APP_LOG_DIR/nohup.out 2>&1 &
