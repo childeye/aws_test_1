eb9b58b41308ff53e8d030b21334c0bffb89f0bf
childeye <childeye@uangel.com>
2020-08-28 14:23:47 +0900

    무중단 배포.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,17 +6,17 @@ APP_LIB_DIR=$APP_HOME/lib
 APP_LOG_DIR=$APP_HOME/logs
 APP_CONF_DIR=$APP_HOME/conf
 APP_DEPLOY_DIR=$APP_HOME/zip
-JAR_NAME=bootweb-0.0.1-SNAPSHOT.jar
+ARTIFACT_ID=bootweb
 
 cd $APP_HOME
 
 echo "> Build 파일 복사"
 
-cp APP_DEPLOY_DIR/$JAR_NAME $APP_LIB_DIR/
+cp APP_DEPLOY_DIR/*.jar $APP_LIB_DIR/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl $JAR_NAME | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $ARTIFACT_ID | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
@@ -30,17 +30,19 @@ fi
 
 echo "> 새 어플리케이션 배포"
 
+JAR_NAME=$(ls -tr $APP_LIB_DIR/*.jar | tail -n 1)
+
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $APP_LIB_DIR/$JAR_NAME
+chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-# nohup java -jar $APP_LIB_DIR/$JAR_NAME > $APP_LOG_DIR/nohup.out 2>&1 &
+# nohup java -jar $JAR_NAME > $APP_LOG_DIR/nohup.out 2>&1 &
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,$APP_CONF_DIR/application-real.properties,$APP_CONF_DIR/application-oauth.properties,$APP_CONF_DIR/application-real-db.properties \
     -Dspring.profiles.active=real \
-    $APP_LIB_DIR/$JAR_NAME > $APP_LOG_DIR/nohup.out 2>&1 &
+    $JAR_NAME > $APP_LOG_DIR/nohup.out 2>&1 &
--- scripts/start.sh
+++ scripts/start.sh
@@ -4,16 +4,21 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=freelec-springboot2-webservice
+APP_HOME=/home/ec2-user/app/test_web_1
+APP_REPOSITORY=$APP_HOME/source/aws_test_1
+APP_LIB_DIR=$APP_HOME/lib
+APP_LOG_DIR=$APP_HOME/logs
+APP_CONF_DIR=$APP_HOME/conf
+APP_DEPLOY_DIR=$APP_HOME/zip
+ARTIFACT_ID=bootweb
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+echo "> cp $APP_DEPLOY_DIR/*.jar $APP_LIB_DIR/"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $APP_DEPLOY_DIR/*.jar $APP_LIB_DIR/
 
 echo "> 새 어플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $APP_LIB_DIR/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
@@ -27,6 +32,6 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.config.location=classpath:/application.properties,$APP_CONF_DIR/application-$IDLE_PROFILE.properties,$APP_CONF_DIR/application-oauth.properties,$APP_CONF_DIR/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    $JAR_NAME > $APP_LOG_DIR/nohup.out 2>&1 &
